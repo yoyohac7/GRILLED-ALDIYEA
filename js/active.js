58dea522cd0f598ca3d289776bfd2ca4a0b0f3aa
YoYo🪀 <153456226+yoyohac7@users.noreply.github.com>
2024-09-19 01:26:29 +0300

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,61 @@
+
+
+    if ($.fn.owlCarousel) {
+        // Hero Slider Active Code
+        $(".caviar-hero-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1500,
+            margin: 0,
+            nav: true,
+            navText: ['', '<i class="fa fa-arrow-right" aria-hidden="true"></i>']
+        })
+        var wel_slides = $('.caviar-hero-slides');
+        wel_slides.owlCarousel();
+        wel_slides.on('translate.owl.carousel', function (event) {
+            $('.hero-content h2').removeClass('animated').hide();
+            $('.hero-content p').removeClass('animated').hide();
+            $('.hero-content .btn').removeClass('animated').hide();
+            $('.hero-slides-nav, .caviar-hero-area .owl-next').removeClass('animated').hide();
+        })
+        wel_slides.on('translated.owl.carousel', function (event) {
+            $('.hero-content h2').addClass('animated fadeIn').show();
+            $('.hero-content p').addClass('animated fadeIn').show();
+            $('.hero-content .btn').addClass('animated fadeIn').show();
+            $('.hero-slides-nav, .caviar-hero-area .owl-next').addClass('animated fadeInUp').show();
+        })
+        // Testimonials Slider Active Code
+        $(".caviar-testimonials-slides").owlCarousel({
+            items: 3,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1500,
+            margin: 0,
+            center: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 2
+                },
+                992: {
+                    items: 2
+                }
+            }
+        })
+        // Menu Slider Active Code
+        $(".caviar-menu-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1500,
+            autoplayTimeout: 12000,
+            margin: 0,
+            nav: true,
+            navText: ['<i class="fa fa-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-arrow-right" aria-hidden="true"></i>']
+        })
+    }
+
+
---
+++ js/custom.js
@@ -0,0 +1,82 @@
+// صفحة الانتظار اثناء تحميل عناصر الصفحة
+const preloader = document.querySelector('#preloader');
+if (preloader) {
+  window.addEventListener('load', () => {
+    preloader.remove();
+  });
+}
+
+    // فلترة اصناف الطعام 
+    $(window).on('load', function () {
+      $('.filters_menu li').click(function () {
+        var filter = $(this).data('filter');
+
+        $('.filters_menu li').removeClass('active');
+        $(this).addClass('active');
+
+        if (filter === '*') {
+          $('.box').show();
+        } else {
+          $('.box').hide();
+          $(filter).show();
+        }
+      });
+
+      var $grid = $(".grid").isotope({
+        itemSelector: ".box",  
+        percentPosition: false,
+        masonry: {
+          columnWidth: ".box"  
+        }
+      });
+
+      // العرض التلقائي لكل اصناف الطعام
+      $('.filters_menu li.active').click();
+    });
+
+
+
+// دوران العروض
+$(document).ready(function () {
+  $('.owl-collection').owlCarousel({
+    items: 3,
+    loop: true,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 2
+      },
+      800: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
+});
+
+
+
+// زر الصعود
+
+let scrollTop = document.querySelector('.scroll-top');
+
+function toggleScrollTop() {
+  if (scrollTop) {
+    window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  }
+}
+scrollTop.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+window.addEventListener('load', toggleScrollTop);
+document.addEventListener('scroll', toggleScrollTop);
